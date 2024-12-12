7fbcb77f7681cfbc357d09555c626a6386895380
nishi240masa <masa2401106@gmail.com>
2024-12-12 15:59:09 +0900

    Add:仮データから棒グラフを描画できるようにした

--- routes/graph.py
+++ routes/graph.py
@@ -1,20 +1,53 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for # type: ignore
 from datetime import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+import base64
+from matplotlib.figure import Figure
+from io import BytesIO
 
 # Blueprintの作成
-article_bp = Blueprint('article', __name__, url_prefix='/graph')
+graph_bp = Blueprint('graph', __name__, url_prefix='/graph')
 
-
-@article_bp.route('/enn')
-def enn():
+@graph_bp.route('/')
+def list():
+    
+    # 棒グラフの画像を取得
+    bouData = bou()
     
 
-    return render_template('graph.html')
+
+    
+    return render_template('graph.html' , bou=bouData)
 
 
-@article_bp.route('/bou',)
+# def enn():
+    
+
+#     return enn
+
+
 def bou():
-
+    
+    labels = ['1', '10', '20', '30', '40']
+    means = [20, 34, 30, 35, 27]
+    x = np.arange(len(labels))
+    width = 0.35
 
     
-    return render_template('graph.html')
+    # グラフ描画
+    fig = Figure()
+    ax = fig.subplots()
+    ax.bar(x - width/2, means, width, label='A')
+    
+    # 画像をバッファに保存
+    buf = BytesIO()
+    fig.savefig(buf, format="png")
+    buf.seek(0)  # バッファのポインタを先頭に戻す
+    
+    # 画像データをBase64にエンコード
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    buf.close()  # バッファを閉じる
+
+#エンコードしたデータを返す
+    return  data
