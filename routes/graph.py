0d4807e6990c8dd561c994c592ec39ce413ffe59
Yosidragon <k23139kk@aitech.ac.jp>
2024-12-12 16:02:14 +0900

    円グラフの描画と受け渡し部分の作成
    うにゃ〜！！

--- routes/graph.py
+++ routes/graph.py
@@ -1,5 +1,10 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from datetime import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
+
 
 # Blueprintの作成
 article_bp = Blueprint('article', __name__, url_prefix='/graph')
@@ -8,8 +13,21 @@ article_bp = Blueprint('article', __name__, url_prefix='/graph')
 @article_bp.route('/enn')
 def enn():
     
-
-    return render_template('graph.html')
+    count_tag_list = [10, 20, 30, 40, 50]
+    
+    labels = ['Python', 'Ruby', 'Java', 'PHP', 'JavaScript']
+    plt.pie(count_tag_list, labels=labels, autopct='%.f%%', labeldistance=None)
+    plt.legend()
+    plt.title('Language')
+    
+    buf = BytesIO()
+    plt.savefig(buf, format="png")
+    
+    #画像データをBase64にエンコード
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    
+    #エンコードしたデータをHTMLに渡す
+    return render_template('graph.html', data=data)
 
 
 @article_bp.route('/bou',)
